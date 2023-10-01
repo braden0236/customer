1f16aa98c9f20e2f1c8dfb5d62169d24a9883c9d
Braden <ytr0236@gmail.com>
2023-10-01 14:37:02 +0000

    refactor: customer route

---
+++ Endpoints/CustomerEndpoint.cs
@@ -0,0 +1,74 @@
+using customer.Contracts.Requests;
+using customer.Extensions;
+using customer.Services;
+
+namespace customer.Endpoints;
+
+public static class CustomerEndpoint
+{
+    public static RouteGroupBuilder MapCustomerApi(this RouteGroupBuilder group)
+    {
+        group.MapGet("/{id:guid}", async (Guid id, ICustomerService _service) =>
+        {
+            var customer = await _service.GetAsync(id);
+            if (customer == null)
+            {
+                return Results.NoContent();
+            }
+
+            return Results.Ok(customer.ToCustomerResponse());
+        });
+
+        group.MapGet("/", async (ICustomerService _service) =>
+        {
+            var customers = await _service.GetAllAsync();
+            if (customers == null)
+            {
+                return null;
+            }
+
+            return customers.Select(c => c.ToCustomerResponse());
+        });
+
+        group.MapPost("/", async (CreateCustomerRequest req, ICustomerService _service) =>
+        {
+            var customer = req.ToCustomer();
+            var result = await _service.CreateAsync(customer);
+            if (!result)
+            {
+                return Results.BadRequest();
+            }
+
+            return Results.StatusCode(StatusCodes.Status201Created);
+        });
+        group.MapPut("/{id:guid}", async (Guid id, UpdateCustomerRequest req, ICustomerService _service) =>
+        {
+            var existingCustomer = await _service.GetAsync(id);
+            if (existingCustomer is null)
+            {
+                return Results.NotFound();
+            }
+
+            var customer = req.ToCustomer(id);
+            var updated = await _service.UpdateAsync(customer);
+            if (!updated)
+            {
+                return Results.NoContent();
+            }
+
+            return Results.Ok();
+        });
+        group.MapDelete("/{id:guid}", async (Guid id, ICustomerService _service) =>
+        {
+            var deleted = await _service.DeleteAsync(id);
+            if (!deleted)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.NoContent();
+        });
+
+        return group;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-using customer.Contracts.Requests;
 using customer.Database;
-using customer.Extensions;
+using customer.Endpoints;
 using customer.Repositories;
 using customer.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
 builder.Services.AddDbContext<CustomerContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("CustomerDB"));
@@ -15,67 +15,9 @@ builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 var app = builder.Build();
+app.MapHealthChecks("/healthz");
 
-app.MapGet("/customer/{id:guid}", async (Guid id, ICustomerService _service) =>
-{
-    var customer = await _service.GetAsync(id);
-    if (customer == null)
-    {
-        return Results.NoContent();
-    }
-
-    return Results.Ok(customer.ToCustomerResponse());
-});
-
-app.MapPost("/customer", async (CreateCustomerRequest req, ICustomerService _service) =>
-{
-    var customer = req.ToCustomer();
-    var result = await _service.CreateAsync(customer);
-    if (!result)
-    {
-        return Results.BadRequest();
-    }
-
-    return Results.StatusCode(StatusCodes.Status201Created);
-});
-
-app.MapGet("/customer", async (ICustomerService _service) =>
-{
-    var customers = await _service.GetAllAsync();
-    if (customers == null)
-    {
-        return null;
-    }
-
-    return customers.Select(c => c.ToCustomerResponse());
-});
-
-app.MapPut("/customer/{id:guid}", async (Guid id, UpdateCustomerRequest req, ICustomerService _service) =>
-{
-    var existingCustomer = await _service.GetAsync(id);
-    if (existingCustomer is null) {
-        return Results.NotFound();
-    }
-
-    var customer = req.ToCustomer(id);
-    var updated = await _service.UpdateAsync(customer);
-    if (!updated) {
-        return Results.NoContent();
-    }
-
-    return Results.Ok();
-});
-
-app.MapDelete("/customer/{id:guid}", async (Guid id, ICustomerService _service) =>
-{
-    var deleted = await _service.DeleteAsync(id);
-    if (!deleted)
-    {
-        return Results.NotFound();
-    }
-
-    return Results.NoContent();
-});
+app.MapGroup("/v1/customer").MapCustomerApi().AllowAnonymous().WithTags("Public");
 
 using (var scope = app.Services.CreateScope())
 {
